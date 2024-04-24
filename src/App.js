214c7ca2f3979c29e6df16f10b5e8cce04b7a458
ishaq_005 <mohamedishaq005@gmail.com>
2024-04-24 22:07:43 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import LoginPage from "./screens/login-screeen";
-import HomePage from "./screens/home-screen";
-import ContactPage from "./screens/contact-screen";
 import LandingPage from "./screens/landing-screen";
-import SignInPage from "./screens/sign-screen";
-import UserPage from "./screens/user-profile";
-import NodeGetApiPage from "./screens/node-get-api";
 import CrudOperationPage from "./screens/crud-operations";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage></LoginPage>}></Route>
-        <Route path="/home" element={<HomePage></HomePage>}></Route>
-        <Route path="/contact" element={<ContactPage></ContactPage>}></Route>
         <Route path="" element={<LandingPage></LandingPage>}></Route>
-        <Route path="signup" element={<SignInPage></SignInPage>}></Route>
-        <Route path="user" element={<UserPage></UserPage>}></Route>
-        <Route
-          path="node-api"
-          element={<NodeGetApiPage></NodeGetApiPage>}
-        ></Route>
         <Route
           path="crud"
           element={<CrudOperationPage></CrudOperationPage>}
--- src/screens/landing-screen.js
+++ src/screens/landing-screen.js
@@ -2,36 +2,9 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const LandingPage = () => {
-  const [myEmail, updateMyEmail] = useState("");
-  const [emailList, updateEmailList] = useState([]);
-
-  const handleEmail = (event) => {
-    updateMyEmail(event.target.value);
-  };
-
-  const addEmail = () => {
-    updateEmailList([...emailList, myEmail]);
-  };
-
-  const email = emailList.map((value, index) => {
-    return <h1 key={index}>{value}</h1>;
-  });
-
   return (
     <div>
       <h1>Landing Page.</h1>
-      <label>Email</label>
-      <input
-        type="email"
-        placeholder="Enter your email"
-        onChange={handleEmail}
-      ></input>
-      <button onClick={() => addEmail()}>Add</button>
-      {email}
-
-      <NavLink to={"/signup"}>SignUp</NavLink>
-      <NavLink to={"/user"}>Go to User screen</NavLink>
-      <NavLink to={"/node-api"}>Go to api page</NavLink>
       <NavLink to={"/crud"}>Goto crud page</NavLink>
     </div>
   );
